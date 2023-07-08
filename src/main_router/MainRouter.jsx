157c5dc9c209a4829c11200e75d050fd7211c60d
unknown <andrey.detochkin@gmail.com>
2023-07-08 15:44:52 +0200

    router and pages update

--- src/main_router/MainRouter.jsx
+++ src/main_router/MainRouter.jsx
@@ -1,9 +1,8 @@
 import { BrowserRouter as Router, Route ,Routes} from 'react-router-dom'
 import Navbar from '../views/NavBar/Navbar'
 import Landing from '../views/Landing/Landing'
-import Page1 from '../views/Page1/Page1'
-import Page2 from '../views/Page2/Page2'
-import Page3 from '../views/Page3/Page3'
+import ShoppingCart from '../views/ShoppingCart/ShoppingCart'
+
 
 
 
@@ -24,9 +23,8 @@ export default function MainRouter() {
         
         <Routes>
               <Route path='/' element = {<Landing/>}/>
-              <Route path='/page1' element = {<Page1/>}/>
-              <Route path='/page2' element = {<Page2/>}/>
-              <Route path='/page3' element = {<Page3/>}/>
+              <Route path='/shopping_cart' element = {<ShoppingCart/>}/>
+            
 
              
 
