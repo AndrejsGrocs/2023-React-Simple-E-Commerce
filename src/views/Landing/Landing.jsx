9392c47af775a92fe58dd5024e4220a60c410920
unknown <andrey.detochkin@gmail.com>
2023-07-08 15:22:28 +0200

    project upload

---
+++ src/views/Landing/Landing.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+export default function Landing() {
+    return (
+      
+        
+          <div>
+            <div className='landing-body'>
+            <h1 className='lp-h1'>The main page of your project</h1>
+           
+        
+          </div>
+        
+          </div>
+
+  
+  
+    );
+  }
