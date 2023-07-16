84763a747ae5f7540bc3f4b8eae0ef8f1edf326f
unknown <andrey.detochkin@gmail.com>
2023-07-16 16:59:37 +0200

    added some shopping cart logic

--- src/products.js
+++ src/products.js
@@ -18,42 +18,42 @@ export const PRODUCTS = [
      },
      {
       id:3,
-      productName: 'Headphones 2',
+      productName: 'Headphones 3',
       price: 250.0,
       productImage: product1
 
    },
    {
       id:4,
-      productName: 'Headphones 2',
+      productName: 'Headphones 4',
       price: 250.0,
       productImage: product1
 
    },
    {
       id:5,
-      productName: 'Headphones 2',
+      productName: 'Headphones 5',
       price: 250.0,
       productImage: product1
 
    },
    {
       id:6,
-      productName: 'Headphones 2',
+      productName: 'Headphones 6',
       price: 250.0,
       productImage: product1
 
    },
    {
       id:7,
-      productName: 'Headphones 2',
+      productName: 'Headphones 7',
       price: 250.0,
       productImage: product1
 
    },
    {
       id:8,
-      productName: 'Headphones 2',
+      productName: 'Headphones 8',
       price: 250.0,
       productImage: product1
 
--- src/views/ShoppingCart/ShoppingCart.jsx
+++ src/views/ShoppingCart/ShoppingCart.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { PRODUCTS } from "../../products";
+import {ShopContext} from "../../Context/context"
+import {CartItem} from "../ShoppingCart/CartItem"
 
 export default function Page1() {
+  const {cartItems} =useContext(ShopContext)
   return (
+    <>
     <div className='page-1-body'>
       <h1 className='p1-h1'>Shopping Cart</h1>
+
+      <div>
+      <div className='cart-items'>
+
+{/* //! Products list
+//! Displaying products only are added to cart */}
+{PRODUCTS.map((product)=>{
+  {/* //! !==0 means that this id is greater than 0 */}
+  if (cartItems[product.id] !== 0){
+    return <CartItem data = {product}/>
+  }
+
+
+})}
+
+</div>
+      </div>
+      
     </div>
+
+ 
+    </>
   )
 }
