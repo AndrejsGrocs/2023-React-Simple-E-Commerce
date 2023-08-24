b4ceb77f5453e8d1bd2b69ce4ed2e895721533ed
unknown <andrey.detochkin@gmail.com>
2023-08-24 16:15:43 +0200

    shopping cart design update

--- src/views/ShoppingCart/CartItem.jsx
+++ src/views/ShoppingCart/CartItem.jsx
@@ -7,19 +7,24 @@ export const CartItem = (props) => {
 
   return (
     <div className='cart-item'>
+    <div className='cart-item-image-section'>
     <img className='cart-item-img' src={productImage}></img>
+
+    </div>
+    
     <div className='description'>
     <p>{productName}</p>
     <p><b>{price} EUR</b></p>
+
+
+
+    </div>
     <div className='count-handler'>
     <button className='cart-item-button' onClick={()=>removeFromCart(id)}>-</button>
     {/* //! cartItems[id] shows the amount of every product in every inputfield */}
     {/* //! (Number(e.target.value) converting string to number */}
     <input className='c_i_input' value={cartItems[id]} onChange={(e)=> updateCartItemCount(Number(e.target.value),id)}/>
     <button className='cart-item-button'  onClick={()=>addToCart(id)}>+</button>
-
-    </div>
-
 
     </div>
 
